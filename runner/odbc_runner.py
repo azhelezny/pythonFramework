16a8bab5731ba8e415644aa102187947d24b7833
azhelezny <azhelezny@gmail.com>
2016-11-23 16:30:48 +0200

    development...

--- runner/odbc_runner.py
+++ runner/odbc_runner.py
@@ -3,11 +3,21 @@ import pyodbc
 from structures.structures import QueryType
 
 
-def run_request(request, variables):
-    conn = pyodbc.connect(
+def open_connection(autocmt=True):
+    return pyodbc.connect(
         "DSN=SpliceODBC64;DRIVER=/home/splice/splice/lib64/libsplice_odbc.so;UID=splice;PWD=admin;URL=10.1.1.222;PORT=1527",
-        autocommit=True)
-    cursor = conn.cursor()
+        autocommit=autocmt)
+
+
+def run_request(conn, request, variables):
+    cursor = ""
+    try:
+        cursor = conn.cursor()
+    except Exception as e:
+        if e.__class__ == pyodbc.ProgrammingError:
+            conn = open_connection(True)
+            cursor = conn.cursor()
+    print "query:" + replace_variables(request.query, variables)
     cursor.execute(replace_variables(request.query, variables))
     row_count = str(cursor.rowcount)
     rows = []
@@ -18,15 +28,12 @@ def run_request(request, variables):
                 rows.append(row)
         except pyodbc.ProgrammingError:
             print "ERROR"
-            return {row_count: []}
+    conn.close()
     return {row_count: rows}
 
 
 def replace_variables(query, variables):
     result = ""
-    print "query:" + query
     for key, value in variables.iteritems():
-        result = query.replace(key, value)
-        print "replacing " + key + " on " + value
-    print "result:" + result
+        result = query.replace("${" + key + "}", value)
     return result
--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,10 +1,12 @@
 import os
 import sys
 
+import pyodbc
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from jmx_parser.JmxParser import JmxParser
-from runner.odbc_runner import run_request
+from runner.odbc_runner import run_request, open_connection
 
 file_path = "../resources/example.jmx"
 
@@ -14,10 +16,13 @@ threads = parser.threads
 variables = parser.variables
 for thread in threads:
     print thread.thread_name
+    conn = open_connection(True)
+    print conn.autocommit
     for request in thread.requests:
         print "REQUEST\n" + request.__str__()
-        results = run_request(request, variables)
+        results = run_request(conn, request, variables)
         print "RESULTS\n"
         for key, value in results.iteritems():
             print key
             print value
+    conn.close()
